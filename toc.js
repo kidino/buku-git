63af7fd952dbbc7e42d644a76845ecd3dbb8a172
Iszuddin Ismail <dino@websegera.my>
2015-12-11 20:53:25 +0800

    toc.js tertinggal
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

---
+++ toc.js
@@ -0,0 +1,156 @@
+var toc = [
+  {
+    "title": "Pengenalan",
+    "slug": "pengenalan"
+  },
+  {
+    "title": "Bahagian 1 - Bekerja Sendiri",
+    "slug": "bahagian_1_-_bekerja_sendiri",
+    "sub": [
+      {
+        "title": "Apa itu Version Control Management",
+        "slug": "apa_itu_version_control_management"
+      },
+      {
+        "title": "Kehidupan Seorang Programmer Tanpa Git",
+        "slug": "kehidupan_seorang_programmer_tanpa_git"
+      },
+      {
+        "title": "Memasang dan Mengenali Git",
+        "slug": "memasang_dan_mengenali_git"
+      },
+      {
+        "title": "Git Init -- Memulakan Projek Baru",
+        "slug": "git_init_--_memulakan_projek_baru"
+      },
+      {
+        "title": "Git Add -- Memilih Fail-fail yang ingin disimpan",
+        "slug": "git_add_--_memilih_fail-fail_yang_ingin_disimpan"
+      },
+      {
+        "title": "Git Ignore -- Memaklumkan pada Git Fail-fail yang perlu diabaikan",
+        "slug": "git_ignore_--_memaklumkan_pada_git_fail-fail_yang_perlu_diabaikan"
+      },
+      {
+        "title": "Git Commit -- Menyimpan Kerja",
+        "slug": "git_commit_--_menyimpan_kerja"
+      },
+      {
+        "title": "Git Tag -- Menandakan Hasil Kerja",
+        "slug": "git_tag_--_menandakan_hasil_kerja"
+      },
+      {
+        "title": "Git Checkout -- Melihat Kerja-kerja Lampau",
+        "slug": "git_checkout_--_melihat_kerja-kerja_lampau"
+      },
+      {
+        "title": "Git Revert -- Buang dan Ulang Semula",
+        "slug": "git_revert_--_buang_dan_ulang_semula"
+      },
+      {
+        "title": "Branching -- Menguruskan Perubahan dalam Kerja-kerja",
+        "slug": "branching_--_menguruskan_perubahan_dalam_kerja-kerja"
+      },
+      {
+        "title": "Git Merge -- Menggabung Kerja-kerja",
+        "slug": "git_merge_--_menggabung_kerja-kerja"
+      },
+      {
+        "title": "Resolve -- Membaiki Konflik dalam Kerja",
+        "slug": "resolve_--_membaiki_konflik_dalam_kerja"
+      },
+      {
+        "title": "Git GUI -- Menyemak Sejarah dan Peta Kerja-kerja",
+        "slug": "git_gui_--_menyemak_sejarah_dan_peta_kerja-kerja"
+      }
+    ]
+  },
+  {
+    "title": "Bahagian 2 - Bekerja dengan Server Repository (seperti Github, Gitlab, Bitbucket)",
+    "slug": "bahagian_2_-_bekerja_dengan_server_repository_seperti_github_gitlab_bitbucket_",
+    "sub": [
+      {
+        "title": "Mendaftar Akaun di Github",
+        "slug": "mendaftar_akaun_di_github"
+      },
+      {
+        "title": "Mempersiapkan Akaun dengan Kunci SSH",
+        "slug": "mempersiapkan_akaun_dengan_kunci_ssh"
+      },
+      {
+        "title": "Membina Sebuah Repo (Repositori) Baru",
+        "slug": "membina_sebuah_repo_repositori_baru"
+      },
+      {
+        "title": "Git Remote -- Hubungkan Kerja Lokal Dengan Kerja di Server",
+        "slug": "git_remote_--_hubungkan_kerja_lokal_dengan_kerja_di_server"
+      },
+      {
+        "title": "Git Clone -- Membina Kerja Lokal Daripada Repo di Server",
+        "slug": "git_clone_--_membina_kerja_lokal_daripada_repo_di_server"
+      },
+      {
+        "title": "Git Pull -- Mengemaskini dengan yang Terkini Daripada Repo di Server",
+        "slug": "git_pull_--_mengemaskini_dengan_yang_terkini_daripada_repo_di_server"
+      },
+      {
+        "title": "Git Push -- Menghantar Kerja-kerja ke Repo di Server",
+        "slug": "git_push_--_menghantar_kerja-kerja_ke_repo_di_server"
+      },
+      {
+        "title": "Git Fetch -- Mendapatkan Branch-branch lain yang terdapat di Repo di Server",
+        "slug": "git_fetch_--_mendapatkan_branch-branch_lain_yang_terdapat_di_repo_di_server"
+      },
+      {
+        "title": "Forking",
+        "slug": "forking"
+      },
+      {
+        "title": "Menghantar Pull/Merge Request",
+        "slug": "menghantar_pull_merge_request"
+      },
+      {
+        "title": "Menerima Pull/Merge Request",
+        "slug": "menerima_pull_merge_request"
+      },
+      {
+        "title": "Membina Release",
+        "slug": "membina_release"
+      },
+      {
+        "title": "Web Hook",
+        "slug": "web_hook"
+      },
+      {
+        "title": "Github Pages",
+        "slug": "github_pages"
+      }
+    ]
+  },
+  {
+    "title": "Bahagian 3 - Pengurusan Projek Dengan Github",
+    "slug": "bahagian_3_-_pengurusan_projek_dengan_github",
+    "sub": [
+      {
+        "title": "Melindungi Branch Penting (master mungkin)",
+        "slug": "melindungi_branch_penting_master_mungkin_"
+      },
+      {
+        "title": "Membina Ahli Pasukan",
+        "slug": "membina_ahli_pasukan"
+      },
+      {
+        "title": "Isu (Issues)",
+        "slug": "isu_issues_"
+      },
+      {
+        "title": "Milestone",
+        "slug": "milestone"
+      },
+      {
+        "title": "Git Push dengan Komen ke Issue",
+        "slug": "git_push_dengan_komen_ke_issue"
+      }
+    ]
+  }
+];
